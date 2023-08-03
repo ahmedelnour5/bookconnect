11abc28f674e120d61b33712f6af19ca9da27385
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-08-03 16:02:49 -0400

    style register page

--- frontend/src/Components/AuthBtns.jsx
+++ frontend/src/Components/AuthBtns.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import '../Styles/Header.css';
+import { useNavigate } from 'react-router-dom';
 
 const AuthBtns = () => {
+  const navigate = useNavigate();
+
+  const handleSignIn = () => {
+    navigate('/login');
+  };
+
+  const handleSignUp = () => {
+    navigate('/register');
+  };
+
   return (
     <div className="authBtnGroup">
-      <Button className="sign-In-Btn" size="medium">
+      <Button className="sign-In-Btn" size="medium" onClick={handleSignIn}>
         Sign in
       </Button>
-      <Button className="sign-Up-Btn" variant="contained" size="medium">
+      <Button
+        className="sign-Up-Btn"
+        variant="contained"
+        size="medium"
+        onClick={handleSignUp}
+      >
         Sign up
       </Button>
     </div>
--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,10 +1,62 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
+import Logo from '../Components/Logo';
+import Button from '@mui/material/Button';
+import { useNavigate } from 'react-router-dom';
+import '../Styles/Register.css';
+
+export const RegisterHeader = () => {
+  return <h2 className="register-heading">Welcome to Readfluence</h2>;
+};
+export const RegisterForm = () => {
+  return (
+    <div className="form-container">
+      <form className="form">
+        <input type="text" placeholder="Name" name="name"></input>
+        <input type="text" placeholder="E-mail" name="email"></input>
+        <input type="password" placeholder="Password" name="password"></input>
+        <input
+          type="password"
+          placeholder="Confirm Password"
+          name="password2"
+        ></input>
+        <Button variant="contained" fullWidth={true} className="submitBtn">
+          Create a free account
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export const RegisterNav = () => {
+  const navigate = useNavigate();
+  const handleClick = () => {
+    navigate('/login');
+  };
+
+  return (
+    <nav className="registerNav">
+      <div className="registerNav-container">
+        <Logo />
+        <div className="registerNav-right-container">
+          <span>Already have an account?</span>
+          <Button variant="outlined" size="medium" onClick={handleClick}>
+            Sign in
+          </Button>
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 const Register = () => {
   return (
     <div className="Register">
-      <Paper className="form"></Paper>
+      <RegisterNav />
+      <div className="register-container">
+        <RegisterHeader />
+        <RegisterForm />
+      </div>
     </div>
   );
 };
