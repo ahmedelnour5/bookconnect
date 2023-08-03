bc3a6a7c09ddd107e8dae7ec1e737ff996ccd41d
Ahmed Elnour <ahmedelnour@Ahmeds-Air.fios-router.home>
2023-08-03 18:07:13 -0400

    Add input component and register functionality

--- frontend/src/Pages/Register.jsx
+++ frontend/src/Pages/Register.jsx
@@ -1,30 +1,108 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Paper from '@mui/material/Paper';
 import Logo from '../Components/Logo';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import '../Styles/Register.css';
+import { registerUser } from '../features/Auth';
 
 export const RegisterHeader = () => {
   return <h2 className="register-heading">Welcome to Readfluence</h2>;
 };
-export const RegisterForm = () => {
+
+export const Input = ({ type, placeholder, id, name, value, onChange }) => {
   return (
-    <div className="form-container">
+    <input
+      type={type}
+      placeholder={placeholder}
+      id={id}
+      name={name}
+      value={value}
+      onChange={onChange}
+    ></input>
+  );
+};
+export const RegisterForm = () => {
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  });
+
+  const { name, email, password, password2 } = formData;
+
+  const handleChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== password2) {
+      alert('Passwords dont match');
+    } else {
+      const userData = {
+        name,
+        email,
+        password,
+      };
+
+      await Auth.registerUser(userData);
+    }
+  };
+
+  return (
+    <Paper elevation={5} className="form-container">
       <form className="form">
-        <input type="text" placeholder="Name" name="name"></input>
-        <input type="text" placeholder="E-mail" name="email"></input>
-        <input type="password" placeholder="Password" name="password"></input>
-        <input
+        <label>Name:</label>
+        <Input
+          type="text"
+          placeholder="Name"
+          id="Name"
+          name="name"
+          value={name}
+          onChange={handleChange}
+        />
+        <label>Email:</label>
+        <Input
+          type="text"
+          placeholder="Email"
+          id="email"
+          name="email"
+          value={email}
+          onChange={handleChange}
+        />
+        <label>Password:</label>
+        <Input
+          type="password"
+          placeholder="Password"
+          id="password"
+          name="password"
+          value={password}
+          onChange={handleChange}
+        />
+        <label>Confirm Password:</label>
+        <Input
           type="password"
           placeholder="Confirm Password"
+          id="password2"
           name="password2"
-        ></input>
-        <Button variant="contained" fullWidth={true} className="submitBtn">
-          Create a free account
+          value={password2}
+          onChange={handleChange}
+        />
+        <Button
+          variant="contained"
+          fullWidth={true}
+          className="submitBtn"
+          size="medium"
+        >
+          Create free account
         </Button>
       </form>
-    </div>
+    </Paper>
   );
 };
 
